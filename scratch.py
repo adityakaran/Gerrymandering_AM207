4fe6f4ab9d6d17cd18f323584d7c42c5c899d509
adityakaran <akaran@caltech.edu>
2018-12-07 15:37:31 -0500

    Adding county split function

--- scratch.py
+++ scratch.py
@@ -4,8 +4,8 @@ Created on Sat Dec  1 21:53:19 2018
 
 @author: Aditya
 """
-from gerrychain import Graph, Partition
-from gerrychain.updaters import Tally
+from gerrychain import Graph, Partition, GeographicPartition
+from gerrychain.updaters import Tally,county_splits
 import os
 
 from gerrychain import MarkovChain
@@ -15,6 +15,8 @@ from gerrychain.accept import always_accept
 import gerrychain.scores
 from gerrychain import Election
 
+from collections import Counter
+import numpy as np
 
 
 def generate_graph(path):
@@ -34,7 +36,7 @@ def run_simple(graph):
             assignment="2011_PLA_1",
             updaters={
                 "2016_President": election,
-                "population": Tally("TOT_POP", alias="population")
+                "population": Tally("TOT_POP", alias="population"), 
             }
         )
         
@@ -55,10 +57,75 @@ def run_simple(graph):
         wins.append(partition["2016_President"].wins("Democratic"))
     
     
-    
-filename = "PA_VTD.shp"
-direc = "PA_VTD"
-fullpath = os.path.join(direc, filename)
-graph = Graph.from_file(fullpath)
-run_simple(graph)
+   
 
+#
+#filename = "PA_VTD.shp"
+#direc = "PA_VTD"
+#fullpath = os.path.join(direc, filename)
+#graph = Graph.from_file(fullpath)
+#run_simple(graph)
+#
+#
+#p2 = Partition(
+#        graph,
+#        assignment="2011_PLA_1",
+#        updaters={
+#            "2016_President": election,
+#            "population": Tally("TOT_POP", alias="population"), 
+#            "county_split" : county_splits( 'HI', "COUNTYFP10")
+#        }
+#    )
+
+def county_split_wrapper(partition, county_split_name = 'county _split', district_name = 'district_name'):
+    splits = county_split_score(partition, county_split_name)
+    score = compute_countySplitWeight(partition, splits, county_split_name, district_name)
+    return(score)
+    
+def county_split_score(partition, county_split_name = 'county_split'):
+    splits = partition[county_split_name]
+    to_check = {}
+    for s in splits:
+        num_split = len(splits[s][2])
+        if(num_split > 1):
+            prev = to_check.get(num_split, [])
+            prev.append(s)
+            to_check[num_split] = prev
+    return(to_check)
+
+def compute_countySplitWeight(partition, info, county_split_name = 'county_split', district_name = "2011_PLA_1"):
+    '''Compute the county score function as described by the paper
+    Takes in a parition with a county score_split data and computes the final 
+    score'''
+    two_counties = info[2]
+    two_county_weight_sum = 0
+    '''FIxme - should really combine both parts'''
+    for c in two_counties:        
+        nodes = partition[county_split_name][c][1]
+        counties = [partition.graph.nodes[n][district_name] for n in nodes]
+        cn = Counter(counties)
+        second_frac = cn.most_common(2)[1][1]/ sum(cn.values())
+        two_county_weight_sum += np.sqrt(second_frac)
+    
+    three_plus_weight_sum = 0
+    for count_counties in range(3, len(info) +1):
+        counties_split_list = info[count_counties]
+        for c in counties_split_list:
+            nodes = partition[county_split_name][c][1]
+            counties = [partition.graph.nodes[n][district_name] for n in nodes]
+            cn = Counter(counties)
+            temp_frac = 0
+            lst = cn.most_common()
+            for i in range(2, len(lst)):
+                temp_frac += lst[i][1]
+            temp_frac = temp_frac / sum(cn.values())
+            three_plus_weight_sum += np.sqrt(temp_frac)
+    
+    # Comput the 2 county split weight vs the 3 
+    num_2 = len(two_counties)
+    flat_list = [item for sublist in list(info.values()) for item in sublist]
+    num_3plus = len(flat_list) - num_2
+    final_score = two_county_weight_sum * num_2 + num_3plus * three_plus_weight_sum * 1000
+    if(final_score < 0):
+        raise Exception("FInal weight should be less than 0")
+    return(final_score)
